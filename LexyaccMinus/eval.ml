2d875564b99d5a5ed31594820e3b69979a63dcd5
mathias-berry <mathias.berry@ens-lyon.fr>
2021-02-26 18:01:26 +0100

    eval fonction

--- LexyaccMinus/eval.ml
+++ LexyaccMinus/eval.ml
@@ -7,6 +7,33 @@ let rec recup e s = match e with
 	| (a, b)::q when a <> s -> recup q s
 	| (a, b)::q -> b
 
+let recupfonc  e =
+  match e with
+    | Function (x,f)-> (x,f)
+    | _ -> failwith"e n'est pas une fonction"
+
+let rec remplce x e f =
+  match f with
+  | Const k -> Const k
+  | Add(e1,e2) -> Add(remplce x e e1,remplce x e e2)
+  | Mul(e1,e2) -> Mul(remplce x e e1,remplce x e e2)
+  | Div(e1,e2) -> Div(remplce x e e1,remplce x e e2)
+  | Min(e1,e2) -> Min(remplce x e e1,remplce x e e2)
+  | Variable s -> if Variable s = x then e else Variable s
+  | Letin (a, b, c) -> Letin(a, remplce x e b, remplce x e c)
+  | Ifte (a,b,c) -> Ifte(remplce x e a,remplce x e b, remplce x e c)
+  | Lt (a,b) -> Lt(remplce x e a,remplce x e b)
+  | Le (a,b) -> Le(remplce x e a,remplce x e b)
+  | Gt (a,b) -> Gt(remplce x e a,remplce x e b)
+  | Ge (a,b) -> Ge(remplce x e a,remplce x e b)
+  | Eg (a,b) -> Eg(remplce x e a,remplce x e b)
+  | And (a,b) -> And(remplce x e a,remplce x e b)
+  | Or (a,b) -> Or(remplce x e a,remplce x e b)
+  | Non (a) -> Non(remplce x e a)
+  | Print (a) -> Print(remplce x e a)
+  | Foction (a,b) -> Foction(remplce x e a,remplce x e b)
+  | Appli (a,b) -> Appli(remplce x e a,remplce x e b)*
+
 
 let rec eval env = function
   | Const k -> k
@@ -19,6 +46,9 @@ let rec eval env = function
   | Ifte(e1, e2, e3) -> if eval_bool env e1 then eval env e2 else eval env e3
   | Print (a) -> (let b = eval env a in
                   print_int b;print_newline (); b)
+  | Appli (e1, e2) -> let (x,f) = recupfonc e1 in
+                      eval env (remplce x e2 f)
+
   | _ -> failwith "bite"
 
 and eval_bool env = function
--- LexyaccMinus/expr.ml
+++ LexyaccMinus/expr.ml
@@ -41,16 +41,16 @@ let rec affiche_expr e =
   | Min(e1,e2) -> aff_aux "Min(" e1 e2
   | Variable s -> print_string s
   | Letin (a, b, c) -> (print_string "let "; affiche_expr a; print_string " = "; affiche_expr b; print_string " in "; affiche_expr c)
-  |Ifte (a,b,c) -> (print_string "if "; affiche_expr a ; print_string " then "; affiche_expr b; print_string " else "; affiche_expr c)
-  |Lt (a,b) ->(affiche_expr a; print_string " < "; affiche_expr b) 
-  |Le (a,b) ->(affiche_expr a; print_string " <= "; affiche_expr b)
-  |Gt (a,b) ->(affiche_expr a; print_string " > "; affiche_expr b)
-  |Ge (a,b) ->(affiche_expr a; print_string " >= "; affiche_expr b)
-  |Eg (a,b) ->(affiche_expr a; print_string " = "; affiche_expr b)
-  |And (a,b) ->(affiche_expr a; print_string " && "; affiche_expr b)
-  |Or (a,b) ->(affiche_expr a; print_string " || "; affiche_expr b)
-  |Non (a) ->(print_string "Not "; affiche_expr a)
-  |Print (a) ->(print_string "Print "; affiche_expr a)
-  |Foction (a,b) ->
-  
+  | Ifte (a,b,c) -> (print_string "if "; affiche_expr a ; print_string " then "; affiche_expr b; print_string " else "; affiche_expr c)
+  | Lt (a,b) ->(affiche_expr a; print_string " < "; affiche_expr b) 
+  | Le (a,b) ->(affiche_expr a; print_string " <= "; affiche_expr b)
+  | Gt (a,b) ->(affiche_expr a; print_string " > "; affiche_expr b)
+  | Ge (a,b) ->(affiche_expr a; print_string " >= "; affiche_expr b)
+  | Eg (a,b) ->(affiche_expr a; print_string " = "; affiche_expr b)
+  | And (a,b) ->(affiche_expr a; print_string " && "; affiche_expr b)
+  | Or (a,b) ->(affiche_expr a; print_string " || "; affiche_expr b)
+  | Non (a) ->(print_string "Not "; affiche_expr a)
+  | Print (a) ->(print_string "Print "; affiche_expr a)
+  | Foction (a,b) ->(print_string f"fun"; affiche_expr a; print_string "->"; affiche_expr b)
+  | Appli (a,b) ->(affiche_expr a; print_string " "; affiche_expr b)
 (* sémantique opérationnelle à grands pas *)

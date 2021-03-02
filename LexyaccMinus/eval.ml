566ab98ca79e7fb46bfcf92b228b580886d54372
mathias-berry <mathias.berry@ens-lyon.fr>
2021-03-02 11:55:05 +0100

    modif de Daniel

--- LexyaccMinus/eval.ml
+++ LexyaccMinus/eval.ml
@@ -1,5 +1,25 @@
 open Expr
 
+type envi = (string*value) list
+and value = Int of int | Fun of envi*string*expr*(string option)
+
+let arithop2fun = function
+  | Add -> fun x y -> x + y
+  | Min -> fun x y -> x - y
+  | Mul -> fun x y -> x * y
+  | Div -> fun x y -> x / y
+
+let boolop12fun = function
+  | Eg -> fun x y -> (x = y)
+  | Ge -> fun x y -> (x >= y)
+  | Gt -> fun x y -> (x > y)
+  | Le -> fun x y -> (x <= y)
+  | Lt -> fun x y -> (x < y)
+
+let boolop22fun = function
+  | Or -> fun x y -> x || y
+  | And -> fun x y -> x && y
+
 let rec recup e s = match e with
 	| [] -> failwith "La variable n'est pas définie"
 	| (a, b)::q when a <> s -> recup q s
@@ -31,36 +51,10 @@ let estfun e =
     |_ -> false
 
 
-let rec remplce x e f =
-  match f with
-  | Const k -> Const k
-  | Add(e1,e2) -> Add(remplce x e e1,remplce x e e2)
-  | Mul(e1,e2) -> Mul(remplce x e e1,remplce x e e2)
-  | Div(e1,e2) -> Div(remplce x e e1,remplce x e e2)
-  | Min(e1,e2) -> Min(remplce x e e1,remplce x e e2)
-  | Variable s -> if Variable s = x then e else Variable s
-  | Letin (a, b, c) -> Letin(a, remplce x e b, remplce x e c)
-  | Ifte (a,b,c) -> Ifte(remplce x e a,remplce x e b, remplce x e c)
-  | Lt (a,b) -> Lt(remplce x e a,remplce x e b)
-  | Le (a,b) -> Le(remplce x e a,remplce x e b)
-  | Gt (a,b) -> Gt(remplce x e a,remplce x e b)
-  | Ge (a,b) -> Ge(remplce x e a,remplce x e b)
-  | Eg (a,b) -> Eg(remplce x e a,remplce x e b)
-  | And (a,b) -> And(remplce x e a,remplce x e b)
-  | Or (a,b) -> Or(remplce x e a,remplce x e b)
-  | Non (a) -> Non(remplce x e a)
-  | Print (a) -> Print(remplce x e a)
-  | Fonction (a,b) -> Fonction(remplce x e a,remplce x e b)
-  | Appli (a,b) -> Appli(remplce x e a,remplce x e b)
-
-
 let rec eval env = function
   | Const k -> k
-  | Add(e1,e2) -> (eval env e1) + (eval env e2)
-  | Mul(e1,e2) -> (eval env e1) * (eval env e2)
-  | Min(e1,e2) -> (eval env e1) - (eval env e2)
-  | Div(e1,e2) -> (eval env e1) / (eval env e2)
-  | Letin(Variable s, b, c) -> if estfun b then 
+  | Arithop(op,e1,e2) -> (arithop2fun op) (eval env e1) (eval env e2)
+  | Letin(s, b, c) -> if estfun b then 
                                 begin
                                   let (x,f)= recupfonc b in
                                   eval ( (s,Fun (env,x,f)):: env ) c
@@ -69,31 +63,30 @@ let rec eval env = function
                                begin
                                 eval ( (s,Int  (eval env b)):: env ) c
                                end
+  | Letrec(s, b, c) -> let (x,f) = recupfonc b in
+                                eval ( (s,Fun (env,x,f)):: env ) c
   | Variable s -> recupint (recup env s)
   | Ifte(e1, e2, e3) -> if eval_bool env e1 then eval env e2 else eval env e3
   | Print (a) -> (let b = eval env a in
                   print_int b;print_newline (); b)
   | Appli (e1, e2) -> if estfun e1 then
                         begin
+                          let v2 = eval env e2 in
                           let (x,f) = recupfonc e1 in
-                          eval env (remplce x e2 f)
+                          eval ((x,v2)::env) f
                          end
                       else 
                         begin
                           let v = recupvar e1 in
                           let a = recup env v in
                           let (envi,x,f) = recupfun a in
-                          eval envi (remplce x e2 f)
+                          let v2 = eval env e2 in
+                          eval ((x,v2)::envi) f
                         end
   | _ -> failwith "bite"
 
 and eval_bool env = function
-  | Lt(e1, e2) -> (eval env e1) < (eval env e2)
-  | Le(e1, e2) -> (eval env e1) <= (eval env e2)
-  | Eg(e1, e2) -> (eval env e1) = (eval env e2)
-  | Ge(e1, e2) -> (eval env e1) >= (eval env e2)
-  | Gt(e1, e2) -> (eval env e1) > (eval env e2)
-  | Or(e1, e2) -> (eval_bool env e1) || (eval_bool env e2)
-  | And(e1, e2) -> (eval_bool env e1) && (eval_bool env e2)
+  | Boolop1 (op, e1, e2) -> (boolop12fun op) (eval env e1) (eval env e2)
+  | Boolop2 (op, e1, e2) -> (boolop22fun op) (eval_bool env e1) (eval_bool env e2)
   | Non(e) -> not (eval_bool env e)
   | _ -> failwith "Stade toulousain champion de france"
--- LexyaccMinus/expr.ml
+++ LexyaccMinus/expr.ml
@@ -1,61 +1,63 @@
-(* un type pour des expressions arithmétiques simples *)
+(* un type pour des expressions arithmÃ©tiques simples *)
+type arithop = 
+  Add | Mul |Div |Min
+
+
+type boolop1 =
+   Eg | Ge | Gt | Le | Lt 
+
+type boolop2 =
+   Or | And
 
 type expr =
-    Const of int
-  | Add of expr*expr
-  | Mul of expr*expr
-  | Div of expr*expr
-  | Min of expr*expr
-  | Letin of expr*expr*expr
+  | Const of int
+  | Arithop of arithop*expr*expr
+  | Letin of string*expr*expr
   | Variable of string
   | Ifte of expr*expr*expr
-  | Lt of expr*expr
-  | Le of expr*expr
-  | Gt of expr*expr
-  | Ge of expr*expr
-  | Eg of expr*expr
-  | And of expr*expr 
-  | Or of expr*expr
+  | Boolop1 of boolop1*expr*expr
+  | Boolop2 of boolop2*expr*expr
   | Non of expr
   | Print of expr
-  | Fonction of expr*expr
+  | Fonction of string*expr
   | Appli of expr*expr
+  | Letrec of string*string*expr*expr
 
-type envi = (string*value) list
-and value = Int of int | Fun of envi*expr*expr
+
 
 
 
 
 (* fonction d'affichage *)
+let affichearithop = function
+  | Add -> print_string "Add("
+  | Mul -> print_string "Mul("
+  | Div -> print_string "Div("
+  | Min -> print_string "Min("
+
+let afficheboolop1 = function
+  | Eg -> print_string "Egal("
+  | Lt -> print_string "Lt("
+  | Le -> print_string "Le("
+  | Gt -> print_string "Gt("
+  | Ge -> print_string "Ge("
+
+let afficheboolop2 = function
+  | And -> print_string "And("
+  | Or -> print_string "Or("
+
+
 let rec affiche_expr e =
-  let aff_aux s a b = 
-      begin
-	print_string s;
-	affiche_expr a;
-	print_string ", ";
-	affiche_expr b;
-	print_string ")"
-      end
-  in
   match e with
   | Const k -> print_int k
-  | Add(e1,e2) -> aff_aux "Add(" e1 e2
-  | Mul(e1,e2) -> aff_aux "Mul(" e1 e2
-  | Div(e1,e2) -> aff_aux "Div(" e1 e2
-  | Min(e1,e2) -> aff_aux "Min(" e1 e2
+  | Arithop (op,a,b) -> affichearithop op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
   | Variable s -> print_string s
   | Letin (a, b, c) -> (print_string "let "; affiche_expr a; print_string " = "; affiche_expr b; print_string " in "; affiche_expr c)
   | Ifte (a,b,c) -> (print_string "if "; affiche_expr a ; print_string " then "; affiche_expr b; print_string " else "; affiche_expr c)
-  | Lt (a,b) ->(affiche_expr a; print_string " < "; affiche_expr b) 
-  | Le (a,b) ->(affiche_expr a; print_string " <= "; affiche_expr b)
-  | Gt (a,b) ->(affiche_expr a; print_string " > "; affiche_expr b)
-  | Ge (a,b) ->(affiche_expr a; print_string " >= "; affiche_expr b)
-  | Eg (a,b) ->(affiche_expr a; print_string " = "; affiche_expr b)
-  | And (a,b) ->(affiche_expr a; print_string " && "; affiche_expr b)
-  | Or (a,b) ->(affiche_expr a; print_string " || "; affiche_expr b)
-  | Non (a) ->(print_string "Not "; affiche_expr a)
-  | Print (a) ->(print_string "Print "; affiche_expr a)
-  | Fonction (a,b) ->(print_string "fun"; affiche_expr a; print_string "->"; affiche_expr b)
+  | Boolop1 (op,a,b) -> afficheboolop1 op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
+  | Boolop2 (op,a,b) -> afficheboolop2 op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
+  | Non (a) ->(print_string "Not("; affiche_expr a;print_string ")")
+  | Print (a) ->(print_string "Print("; affiche_expr a; print_string ")")
+  | Fonction (a,b) ->(print_string "(fun "; print_string a; print_string "->"; affiche_expr b; print_string ")")
   | Appli (a,b) ->(affiche_expr a; print_string "("; affiche_expr b; print_string ") ")
-(* sémantique opérationnelle à grands pas *)
+(* sÃ©mantique opÃ©rationnelle Ã  grands pas *)

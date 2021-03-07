dabd21e7c5d8999d37abe38d32cf5a4d84a9f267
Patoche65 <emile.martinez@ens-lyon.fr>
2021-03-07 16:23:13 +0100

    intermdédiaire

--- LexyaccMinus/main.ml
+++ LexyaccMinus/main.ml
@@ -12,6 +12,8 @@ let compile e =
 (* stdin désigne l'entrée standard (le clavier) *)
 (* lexbuf est un canal ouvert sur stdin *)
 
+let nom_fichier = ref ""
+
 let lexbuf = Lexing.from_channel stdin
 
 (* on enchaîne les tuyaux: lexbuf est passé à Lexer.token,
@@ -21,7 +23,14 @@ let parse () = Parser.main Lexer.token lexbuf
 
 (* la fonction que l'on lance ci-dessous *)
 let calc () =
+  let usage  ="" in
+  let optlist = [("-showsrc", Arg.Set source, "Affiche le programme et enlève les sorties");
+                 ("-debug", Arg.Set debug, "Affiche le programme et affiche les sorties")] in
+   
+  Arg.parse optlist (fun s -> nom_fichier := s) usage;
+  
   try
+      let code = open_in !nom_fichier
       let result = parse () in
       (* Expr.affiche_expr result; print_newline (); flush stdout *)
 	compile result; flush stdout

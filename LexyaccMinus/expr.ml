000e7cf5c33db0d209ac57cab698eec6dedbfa93
Patoche65 <emile.martinez@ens-lyon.fr>
2021-03-07 16:21:16 +0100

    bite

--- LexyaccMinus/expr.ml
+++ LexyaccMinus/expr.ml
@@ -30,36 +30,35 @@ type expr =
 
 (* fonction d'affichage *)
 let affichearithop = function
-  | Add -> print_string "Add("
-  | Mul -> print_string "Mul("
-  | Div -> print_string "Div("
-  | Min -> print_string "Min("
+  | Add -> print_string "+"
+  | Mul -> print_string "*"
+  | Div -> print_string "/"
+  | Min -> print_string "-"
 
 let afficheboolop1 = function
-  | Eg -> print_string "Egal("
-  | Lt -> print_string "Lt("
-  | Le -> print_string "Le("
-  | Gt -> print_string "Gt("
-  | Ge -> print_string "Ge("
+  | Eg -> print_string "="
+  | Lt -> print_string "<"
+  | Le -> print_string "<="
+  | Gt -> print_string ">"
+  | Ge -> print_string ">="
 
 let afficheboolop2 = function
-  | And -> print_string "And("
-  | Or -> print_string "Or("
+  | And -> print_string " && "
+  | Or -> print_string " || "
 
 
 let rec affiche_expr e =
   match e with
   | Const k -> print_int k
-  | Arithop (op,a,b) -> affichearithop op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
+  | Arithop (op,a,b) -> print_string "("; affiche_expr a; affichearithop op ; affiche_expr b; print_string ")"
   | Variable s -> print_string s
   | Letin (a, b, c) -> (print_string "let "; print_string a; print_string " = "; affiche_expr b; print_string " in "; affiche_expr c)
   | Ifte (a,b,c) -> (print_string "if "; affiche_expr a ; print_string " then "; affiche_expr b; print_string " else "; affiche_expr c)
-  | Boolop1 (op,a,b) -> afficheboolop1 op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
-  | Boolop2 (op,a,b) -> afficheboolop2 op ; affiche_expr a; print_string "; "; affiche_expr b; print_string ")"
-  | Non (a) ->(print_string "Not("; affiche_expr a;print_string ")")
-  | Print (a) ->(print_string "Print("; affiche_expr a; print_string ")")
+  | Boolop1 (op,a,b) -> print_string "("; affiche_expr a; afficheboolop1 op ; affiche_expr b; print_string ")"
+  | Boolop2 (op,a,b) -> print_string "("; affiche_expr a; afficheboolop2 op ; affiche_expr b; print_string ")"
+  | Non (a) ->(print_string "not("; affiche_expr a;print_string ")")
+  | Print (a) ->(print_string "prInt("; affiche_expr a; print_string ")")
   | Fonction (a,b) ->(print_string "(fun "; print_string a; print_string "->"; affiche_expr b; print_string ")")
   | Appli (a,b) ->(affiche_expr a; print_string "("; affiche_expr b; print_string ") ")
   | Letrec (a, b, c) -> (print_string "let rec "; print_string a; print_string " = "; affiche_expr b; print_string " in "; affiche_expr c)
-(* sémantique opérationnelle à grands pas *)
 

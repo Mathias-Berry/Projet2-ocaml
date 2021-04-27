4618914dffad0ae1cb66b427454dcbeaa115691f
mathias-berry <mathias.berry@ens-lyon.fr>
2021-04-27 09:33:00 +0200

    def types

--- Rendu3/expr.ml
+++ Rendu3/expr.ml
@@ -44,6 +44,17 @@ type expr =
   | Fst
   | Snd
 
+type Types =
+  |Inte
+  |Boole
+  |Unit
+  |Tout
+  |Fonc of types*types
+  |Liste of types
+  |Tuples of types list
+  |Reff of types
+  |Pasdef of int
+
 
 
 
